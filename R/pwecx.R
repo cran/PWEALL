b482a35e46e2bf2111b1e2cb5db39302144dcaeb
Xiaodong Luo <Xiaodong.Luo@sanofi.com>
2018-10-18 10:30:13 +0000

    version 1.3.0

--- R/pwecx.R
+++ R/pwecx.R
@@ -47,17 +47,50 @@ pwecx<-function(t=seq(0,10,by=0.5),rate1=c(1,0.5),rate2=rate1,
   }
   else if (type==3){
     txone<-rep(1,length(tchange))
-    temp<-fourhr(t=t,rate1=r3,rate2=(r1+r3),rate3=r2,rate4=r2,tchange=tchange,eps=eps)$fx
-    temp1<-fourhr(t=t,rate1=r3,rate2=(r1+r3),rate3=txone,rate4=r2,tchange=tchange,eps=eps)$fx
+    pir2=rp2*r2
+    pir4=(1-rp2)*r4
+    temp<-fourhr(t=t,rate1=r3,rate2=(r1+r3)-pir4,rate3=txone,rate4=pir2,tchange=tchange,eps=eps)$fx
+    temp1<-fourhr(t=t,rate1=r3,rate2=(r1+r3)-pir4,rate3=pir2,rate4=pir2,tchange=tchange,eps=eps)$fx
+
+    a1<-pwe(t=t,rate=r1,tchange=tchange)
+    a13<-pwe(t=t,rate=(r1+r3),tchange=tchange)
+    a4<-pwe(t=t,rate=pir4,tchange=tchange)
+    
+    surv<-a13$surv+a4$surv*temp
+    dist<-1-surv
+    density<-a1$hazard*a13$surv+a4$surv*temp1+a4$hazard*a4$surv*temp
+    cumhazard<--log(surv)
+    hazard<-density/surv
+  }
+  else if (type==4){
+    txone<-rep(1,length(tchange))
+    temp<-fourhr(t=t,rate1=r3,rate2=(r1+r3),rate3=txone,rate4=r2,tchange=tchange,eps=eps)$fx
+    temp1<-fourhr(t=t,rate1=r3,rate2=(r1+r3),rate3=r2,rate4=r2,tchange=tchange,eps=eps)$fx
     temp2<-pwefv2(t=t,rate1=r3,rate2=(r1+r3-r4),tchange=tchange,eps=eps)$f0
     
     a1<-pwe(t=t,rate=r1,tchange=tchange)
     a13<-pwe(t=t,rate=(r1+r3),tchange=tchange)
     a4<-pwe(t=t,rate=r4,tchange=tchange)
     
-    surv<-a13$surv+rp2*temp1+(1-rp2)*a4$surv*temp2
+    surv<-a13$surv+rp2*temp+(1-rp2)*temp2*a4$surv
     dist<-1-surv
-    density<-a1$hazard*a13$surv+rp2*temp+(1-rp2)*a4$hazard*a4$surv*temp2
+    density<-a1$hazard*a13$surv+rp2*temp1+(1-rp2)*temp2*a4$hazard*a4$surv
+    cumhazard<--log(surv)
+    hazard<-density/surv
+  }
+  else if (type==5){
+    txone<-rep(1,length(tchange))
+    temp<-fourhr(t=t,rate1=r3,rate2=(r1+r3),rate3=txone,rate4=r2,tchange=tchange,eps=eps)$fx
+    temp1<-fourhr(t=t,rate1=r3,rate2=(r1+r3),rate3=r2,rate4=r2,tchange=tchange,eps=eps)$fx
+    temp2<-fourhr(t=t,rate1=r3,rate2=(r1+r3),rate3=txone,rate4=r4,tchange=tchange,eps=eps)$fx
+    temp3<-fourhr(t=t,rate1=r3,rate2=(r1+r3),rate3=r4,rate4=r4,tchange=tchange,eps=eps)$fx
+    
+    a1<-pwe(t=t,rate=r1,tchange=tchange)
+    a13<-pwe(t=t,rate=(r1+r3),tchange=tchange)
+
+    surv<-a13$surv+rp2*temp+(1-rp2)*temp2
+    dist<-1-surv
+    density<-a1$hazard*a13$surv+rp2*temp1+(1-rp2)*temp3
     cumhazard<--log(surv)
     hazard<-density/surv
   }
--- R/rpwecx.R
+++ R/rpwecx.R
@@ -1,10 +1,12 @@
 ##########################################################################################
 #  random number generator for PieceWise Exp dist with CROSSOVER effect
 #  version 1.0 (11/04/2016)
+#  version 2.0 (10/10/2017)
+#  version 3.0 (10/17/2018): change the algorithm, add two more types of hybrid crossover and output crossover indicators
 ##########################################################################################
 #rPWECROSSFRx<-function(nr=1,rate1=c(1,0.5),rate2=rate1,rate3=c(0.7,0.4),rate4=rate2,rate5=rate2,tchange=c(0,1),type=1,ISEEDS=c(13,17,19,23,29)){
-rpwecx<-function(nr=1,rate1=c(1,0.5),rate2=rate1,rate3=c(0.7,0.4),rate4=rate2,rate5=rate2,tchange=c(0,1),
-                 type=1,rp2=0.5){
+rpwecx<-function (nr = 1, rate1 = c(1, 0.5), rate2 = rate1, rate3 = c(0.7,0.4), rate4 = rate2, rate5 = rate2, tchange = c(0, 1), type = 1, 
+                   rp2 = 0.5){
   # rate1: hazard before crossover
   # rate2: hazard after crossover
   # rate3: hazard for crossover
@@ -15,45 +17,71 @@ rpwecx<-function(nr=1,rate1=c(1,0.5),rate2=rate1,rate3=c(0.7,0.4),rate4=rate2,ra
   # rp2: re-randomization probability to receive the rescue treatment when semi-markov crossover occurs.
   #      When it happens, the overall hazard will be rp2*rate2(t-s)+(1-rp2)*rate4(t), where rate2 is the hazard 
   #           for the (semi-markov) rescue treatment and rate4 is hazard for the (markov) rescue treatment
-
-  #set.seed(ISEEDS[3])
-  if (min(rate3)<1.0e-08)rate3<-rate3+1.0e-08
-  t3<-rpwe(nr=nr, rate=rate3, tchange=tchange)$r
-  if (type==1){
-    s1t3<-pwe(t=t3,rate=rate1,tchange=tchange)$surv
-    s2t3<-pwe(t=t3,rate=rate2,tchange=tchange)$surv
-    #set.seed(ISEEDS[1])
-    x<-runif(nr)
-    x1<-x*s2t3/s1t3
-    y<-rep(0,nr)
-    y[x>s1t3]<-qpwe(p=1-x[x>s1t3],rate=rate1,tchange=tchange)$q
-    y[x<=s1t3]<-qpwe(p=1-x1[x<=s1t3],rate=rate2,tchange=tchange)$q
+  
+  if (min(rate3) < 1e-08) 
+    rate3 <- rate3 + 1e-08
+  ###indicators for crossover
+  cxind<-matrix(0,nrow=nr,ncol=2)
+  
+  y3 <- rpwe(nr = nr, rate = rate3, tchange = tchange)$r
+  y1 <- rpwe(nr = nr, rate = rate1, tchange = tchange)$r
+  cxind[,1]=cxind[,2]=ifelse(y3<y1,1,0)
+  if (type == 1) {
+    s2y3 <- pwe(t = y3, rate = rate2, tchange = tchange)$surv
+    x <- runif(nr)
+    x1 <- x * s2y3
+    y <- rep(0, nr)
+    y[y3>=y1] <- y1[y3>=y1]
+    y[y3<y1] <- qpwe(p = 1 - x1[y3<y1],rate = rate2,tchange = tchange)$q
   }
-  else if (type==2){
-    s1t3<-pwe(t=t3,rate=rate1,tchange=tchange)$surv
-    #set.seed(ISEEDS[1])
-    x<-runif(nr)
-    x1<-x/s1t3
-    y<-rep(0,nr)
-    y[x>s1t3]<-qpwe(p=1-x[x>s1t3],rate=rate1,tchange=tchange)$q
-    y[x<=s1t3]<-t3[x<=s1t3]+qpwe(p=1-x1[x<=s1t3],rate=rate2,tchange=tchange)$q
+  else if (type == 2) {
+    x <- runif(nr)
+    y <- rep(0, nr)
+    y <- rep(0, nr)
+    y[y3>=y1] <- y1[y3>=y1]
+    y[y3<y1] <- y3[y3<y1]+qpwe(p = 1 - x[y3<y1],rate = rate2,tchange = tchange)$q
   }
-  ###Xiaodong double-checked type=3 on 10/10/2017
-  else if (type==3){
-    s1t3<-pwe(t=t3,rate=rate1,tchange=tchange)$surv
-    s4t3<-pwe(t=t3,rate=(1-rp2)*rate4,tchange=tchange)$surv
-    #set.seed(ISEEDS[1])
-    x<-runif(nr);z<-runif(nr)
-    x1<-x/s1t3;z1<-z*s4t3
-    y<-rep(0,nr)
-    y[x>s1t3]<-qpwe(p=1-x[x>s1t3],rate=rate1,tchange=tchange)$q
-    if (sum(x<=s1t3)>0){
-      atemp<-t3[x<=s1t3]+qpwe(p=1-x1[x<=s1t3],rate=rp2*rate2,tchange=tchange)$q
-      btemp<-qpwe(p=1-z1[x<=s1t3],rate=(1-rp2)*rate4,tchange=tchange)$q
-      y[x<=s1t3]<-pmin(atemp,btemp)
+  else if (type == 3) {
+    s4y3 <- pwe(t = y3, rate = (1 - rp2) * rate4, tchange = tchange)$surv
+    x <- runif(nr)
+    z <- runif(nr)
+    z1 <- z * s4y3
+    y <- rep(0, nr)
+    y <- rep(0, nr)
+    y[y3>=y1] <- y1[y3>=y1]
+    if (sum(y3<y1) > 0) {
+      atemp <- y3[y3<y1] + qpwe(p = 1 - x[y3<y1], 
+                                rate = rp2 * rate2, tchange = tchange)$q
+      btemp <- qpwe(p = 1 - z1[y3<y1], rate = (1 - 
+                                                 rp2) * rate4, tchange = tchange)$q
+      y[y3<y1] <- pmin(atemp, btemp)
+      cxind[y3<y1,2]=cxind[y3<y1,1]*ifelse(atemp<=btemp,1,0)
     }
   }
-  list(r=y)
+  else if (type == 4) {
+    s4y3 <- pwe(t = y3, rate = rate4, tchange = tchange)$surv
+    x <- runif(nr)
+    x1 <-x*s4y3
+    z <- rbinom(nr,size=1,prob=rp2)
+    cxind[,2]=z*cxind[,1]
+    y <- rep(0, nr)
+    y[y3>=y1] <-y1[y3>=y1]
+    y[y3<y1&z==1]<-y3[y3<y1&z==1]+qpwe(p = 1 - x[y3<y1&z==1],rate = rate2,tchange = tchange)$q
+    y[y3<y1&z==0]<-qpwe(p = 1 - x1[y3<y1&z==0],rate = rate4,tchange = tchange)$q
+  }
+  else if (type == 5) {
+    s4y3 <- pwe(t = y3, rate = rate4, tchange = tchange)$surv
+    x <- runif(nr)
+    x1 <-x*s4y3
+    z <- rbinom(nr,size=1,prob=rp2)
+    cxind[,2]=z*cxind[,1]
+    y <- rep(0, nr)
+    y[y3>=y1] <-y1[y3>=y1]
+    y[y3<y1&z==1]<-y3[y3<y1&z==1]+qpwe(p = 1 - x[y3<y1&z==1],rate = rate2,tchange = tchange)$q
+    y[y3<y1&z==0]<-y3[y3<y1&z==0]+qpwe(p = 1 - x[y3<y1&z==0],rate = rate4,tchange = tchange)$q
+  }
+  list(r=y,rx=y3,cxind=cxind,type=type)
 }
+
 
 
